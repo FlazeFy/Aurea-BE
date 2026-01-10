f41214b087d5c127be4ae5a3b05f471f996bcbb3
FlazeFy <flazen.edu@gmail.com>
2026-01-11 00:49:27 +0700

    chore(express): initialize index app

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express, { Application, Request, Response } from 'express'
+import cors from "cors"
+import dotenv from "dotenv"
+
+// Router
+import feedbackRouter from "./routers/feedback.router"
+
+// Load env
+dotenv.config()
+const PORT: string = process.env.PORT || "5555"
+
+// Initialize express
+const app: Application = express()
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+// Routes
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).send("Hello world")
+})
+
+app.use("/api/feedbacks", feedbackRouter)
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Apps at http://localhost:${PORT}`)
+})
